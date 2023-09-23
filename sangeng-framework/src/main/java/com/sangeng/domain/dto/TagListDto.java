d9fbf0646e37146bcdcd3bc576e4d084936705a5
gallantsa <gallantsa@163.com>
2023-09-23 22:11:03 +0800

    后台查询标签列表功能代码实现

---
+++ sangeng-framework/src/main/java/com/sangeng/domain/dto/TagListDto.java
@@ -0,0 +1,18 @@
+package com.sangeng.domain.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TagListDto {
+
+     // 标签名
+     private String name;
+
+     // 备注
+     private String remark;
+}
