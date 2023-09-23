9a3910aa3d13b28881fca974197c698247c62d9d
gallantsa <gallantsa@163.com>
2023-09-23 13:18:00 +0800

    博客后台准备工作

---
+++ sangeng-framework/src/main/java/com/sangeng/mapper/TagMapper.java
@@ -0,0 +1,16 @@
+package com.sangeng.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sangeng.domain.entity.Tag;
+
+
+/**
+ * 标签(Tag)表数据库访问层
+ *
+ * @author makejava
+ * @since 2023-09-23 12:19:00
+ */
+public interface TagMapper extends BaseMapper<Tag> {
+
+}
+
---
+++ sangeng-framework/src/main/java/com/sangeng/service/TagService.java
@@ -0,0 +1,16 @@
+package com.sangeng.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.sangeng.domain.entity.Tag;
+
+
+/**
+ * 标签(Tag)表服务接口
+ *
+ * @author makejava
+ * @since 2023-09-23 12:19:01
+ */
+public interface TagService extends IService<Tag> {
+
+}
+
