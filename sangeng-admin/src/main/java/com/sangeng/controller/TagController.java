cb3cd3871b76d68ae4a9ee9ec6c8431e987dc43b
gallantsa <gallantsa@163.com>
2023-09-24 09:27:25 +0800

    后台修改标签功能代码实现

--- sangeng-admin/src/main/java/com/sangeng/controller/TagController.java
+++ sangeng-admin/src/main/java/com/sangeng/controller/TagController.java
@@ -2,9 +2,11 @@ package com.sangeng.controller;
 
 import com.sangeng.domain.ResponseResult;
 import com.sangeng.domain.dto.AddTagDto;
+import com.sangeng.domain.dto.EditTagDto;
 import com.sangeng.domain.dto.TagListDto;
 import com.sangeng.domain.entity.Tag;
 import com.sangeng.domain.vo.PageVo;
+import com.sangeng.domain.vo.TagVo;
 import com.sangeng.service.TagService;
 import com.sangeng.utils.BeanCopyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,5 +38,18 @@ public class TagController {
     }
 
 
+    @GetMapping("/{id}")
+    public ResponseResult getInfo(@PathVariable Long id) {
+        Tag tag = tagService.getById(id);
+        TagVo tagVo = BeanCopyUtils.copyBean(tag, TagVo.class);
+        return ResponseResult.okResult(tagVo);
+    }
+
+    @PutMapping
+    public ResponseResult edit(@RequestBody EditTagDto editTagDto) {
+        Tag tag = BeanCopyUtils.copyBean(editTagDto, Tag.class);
+        tagService.updateById(tag);
+        return ResponseResult.okResult();
+    }
 
 }
