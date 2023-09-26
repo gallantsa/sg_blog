1f796f741cd22e37534863c7cc1f5b81d2012467
gallantsa <gallantsa@163.com>
2023-09-26 16:26:03 +0800

    后台新增友链代码实现

--- sangeng-admin/src/main/java/com/sangeng/controller/LinkController.java
+++ sangeng-admin/src/main/java/com/sangeng/controller/LinkController.java
@@ -5,9 +5,7 @@ import com.sangeng.domain.entity.Link;
 import com.sangeng.domain.vo.PageVo;
 import com.sangeng.service.LinkService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/content/link")
@@ -20,5 +18,11 @@ public class LinkController {
     public ResponseResult list(Link link, Integer pageNum, Integer pageSize) {
         PageVo pageVo = linkService.selectLinkPage(link, pageNum, pageSize);
         return ResponseResult.okResult(pageVo);
+    }
+
+    @PostMapping
+    public ResponseResult add(@RequestBody Link link) {
+        linkService.save(link);
+        return ResponseResult.okResult();
     }
 }
